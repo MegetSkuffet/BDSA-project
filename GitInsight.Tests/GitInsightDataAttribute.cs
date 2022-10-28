5090177f44e9dc25a6bbeb5800e0ae3ff21b3bac
Philip Mørch <mrphil2105@gmail.com>
2022-10-28 14:56:14 +0200

    Provide attribute for generating substitutes

---
+++ GitInsight.Tests/GitInsightDataAttribute.cs
@@ -0,0 +1,19 @@
+using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+
+namespace GitInsight.Tests;
+
+public class GitInsightDataAttribute : AutoDataAttribute
+{
+    public GitInsightDataAttribute() : base(CreateFixture)
+    {
+    }
+
+    private static IFixture CreateFixture()
+    {
+        var fixture = new Fixture();
+        fixture.Customize(new AutoNSubstituteCustomization { ConfigureMembers = true });
+
+        return fixture;
+    }
+}
