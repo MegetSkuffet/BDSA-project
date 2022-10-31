52108bc8ba54832db8dce97766d869b74f16fe67
Philip Mørch <mrphil2105@gmail.com>
2022-10-31 13:30:09 +0100

    Use LINQ method chain syntax

--- GitInsight/CommitFrequency.cs
+++ GitInsight/CommitFrequency.cs
@@ -2,7 +2,6 @@ namespace GitInsight;
 
 public class CommitFrequency : ICommitFrequency
 {
-
     private readonly IRepository _repository;
 
     public CommitFrequency(IRepository repository)
@@ -12,22 +11,24 @@ public class CommitFrequency : ICommitFrequency
 
     public IEnumerable<string> GetAll()
     {
-        var commitList = _repository.Commits.ToList();
+        var commits = _repository.Commits.ToList();
 
-        return from g in (commitList.GroupBy(i => i.Author.When.Date))
-               select String.Format("{0} {1:dd-MM-yy}", g.Count(), g.Key);
-
+        return commits.GroupBy(c => c.Author.When.Date)
+            .Select(g => string.Format("{0} {1:dd-MM-yy}", g.Count(), g.Key));
     }
 
     public IEnumerable<(string, IEnumerable<string>)> GetGroupedByAuthor()
     {
-        var commitList = _repository.Commits.ToList();
+        var commits = _repository.Commits.ToList();
 
-        var authors = commitList.Select(c => c.Author.Name).Distinct();
+        var authors = commits.Select(c => c.Author.Name)
+            .Distinct();
 
-        foreach (var a in authors)
+        foreach (var author in authors)
         {
-            yield return (a, commitList.Where(c => c.Author.Name == a).GroupBy(i => i.Author.When.Date).Select(g => String.Format("{0} {1:dd-MM-yy}", g.Count(), g.Key)));
+            yield return (author, commits.Where(c => c.Author.Name == author)
+                .GroupBy(i => i.Author.When.Date)
+                .Select(g => String.Format("{0} {1:dd-MM-yy}", g.Count(), g.Key)));
         }
     }
 }
