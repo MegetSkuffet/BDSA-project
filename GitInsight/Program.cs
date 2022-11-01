8bc120461db8f76e84a7e15496cc8b03a6b3cd0d
Philip Mørch <mrphil2105@gmail.com>
2022-11-01 13:07:46 +0100

    Implement print in frequency and author mode

--- GitInsight/Program.cs
+++ GitInsight/Program.cs
@@ -1,3 +1,62 @@
-﻿using LibGit2Sharp;
+﻿using CommandLine;
 
-var repo = new Repository(@"C:\Users\malte\Documents\Exercises\c#\bdsa-assignment-03");
+namespace GitInsight;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var parser = new Parser(s =>
+        {
+            s.EnableDashDash = true;
+            s.HelpWriter = Console.Error;
+        });
+        var result = parser.ParseArguments<Options>(args);
+        var options = result.Value;
+
+        if (result.Tag == ParserResultType.NotParsed)
+        {
+            return;
+        }
+
+        var repository = new Repository(options.RepositoryPath);
+        var insightRepository = new InsightRepository(repository);
+        var frequency = new CommitFrequency(insightRepository);
+
+        if (options.AuthorMode)
+        {
+            PrintAuthorMode(frequency);
+        }
+        else
+        {
+            PrintFrequencyMode(frequency);
+        }
+    }
+
+    private static void PrintFrequencyMode(ICommitFrequency frequency)
+    {
+        var lines = frequency.GetAll();
+
+        foreach (var line in lines)
+        {
+            Console.WriteLine(line);
+        }
+    }
+
+    private static void PrintAuthorMode(ICommitFrequency frequency)
+    {
+        var groupedLines = frequency.GetGroupedByAuthor();
+
+        foreach (var grouping in groupedLines)
+        {
+            Console.WriteLine(grouping.Author);
+
+            foreach (var line in grouping.Lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
