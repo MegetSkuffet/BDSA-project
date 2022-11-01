eec4ae5890bd9602f72e5f95ee99c590983d55a0
Jklo <jklo@itu.dk>
2022-11-01 13:50:35 +0100

    Template for tables created

---
+++ Infrastructure/GitInsightContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class GitInsightContext: DbContext
+{
+    public GitInsightContext(DbContextOptions<GitInsightContext> options)
+        : base(options)
+    {
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+    
+
+
+}
