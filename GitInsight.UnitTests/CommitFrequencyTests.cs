13a36b8dc5826e424efaddf68fe263cf1612479e
Philip Mørch <mrphil2105@gmail.com>
2022-11-02 14:53:52 +0100

    Add additional unit tests for GetAll

--- GitInsight.UnitTests/CommitFrequencyTests.cs
+++ GitInsight.UnitTests/CommitFrequencyTests.cs
@@ -12,4 +12,42 @@ public class CommitFrequencyTests
         result.Should()
             .ContainInOrder(repository.Commits.Select(c => $"{1,7} {c.Author.When:dd-MM-yy}"));
     }
+
+    [Theory]
+    [GitInsightData]
+    public void GetAll_ReturnsFrequenciesAndDates_GivenSameAuthorCommits([Frozen] InsightCommit commit,
+        [Frozen] IInsightRepository repository, CommitFrequency frequency)
+    {
+        var result = frequency.GetAll();
+
+        result.Should()
+            .OnlyContain(s => s == $"{repository.Commits.Count(),7} {commit.Author.When:dd-MM-yy}");
+    }
+
+    [Theory]
+    [GitInsightData]
+    public void GetAll_ReturnsFrequenciesAndDates_GivenMultipleSameAuthorCommits(
+        [Frozen] List<InsightCommit> firstCommits, List<InsightCommit> secondCommits,
+        [Frozen] IInsightRepository repository, CommitFrequency frequency)
+    {
+        repository.Commits.Returns(firstCommits.Concat(secondCommits));
+
+        var result = frequency.GetAll();
+
+        result.Should()
+            .ContainInOrder(repository.Commits.Select(c => $"{2,7} {c.Author.When:dd-MM-yy}")
+                .Distinct());
+    }
+
+    [Theory]
+    [GitInsightData]
+    public void GetAll_ReturnsEmpty_GivenNoCommits([Frozen] IInsightRepository repository, CommitFrequency frequency)
+    {
+        repository.Commits.Returns(Enumerable.Empty<InsightCommit>());
+
+        var result = frequency.GetAll();
+
+        result.Should()
+            .BeEmpty();
+    }
 }
--- GitInsight.UnitTests/Usings.cs
+++ GitInsight.UnitTests/Usings.cs
@@ -1,4 +1,5 @@
 global using AutoFixture.Xunit2;
 global using FluentAssertions;
 global using GitInsight.Abstractions;
+global using NSubstitute;
 global using Xunit;
