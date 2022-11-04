f6e5b1e62ff5f664a17b341bfa5c971322e86773
Jklo <jklo@itu.dk>
2022-11-04 09:26:53 +0100

    context created

--- Infrastructure/GitInsightContext.cs
+++ Infrastructure/GitInsightContext.cs
@@ -8,16 +8,31 @@ public class GitInsightContext: DbContext
         : base(options)
     {
     }
-    public virtual DbSet<AuthorEntity> Authors { get; set; }
+    
+    //Vi behøver vel egentligt slet ikke author, gør vi? Det er en weak entity type der kun har én attribute. At lave
+    //et helt table til det virker skørt.
+    //public virtual DbSet<AuthorEntity> Authors { get; set; }
     public virtual DbSet<CommitEntity> Commits { get; set; }
     public virtual DbSet<RepositoryEntity> Repositories { get; set; }
-
-
-
-
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        
+        modelBuilder.Entity<RepositoryEntity>(r =>
+        {
+            r.Property(p => p.ID).IsRequired();
+            r.HasIndex(p => p.ID).IsUnique();
+            r.HasKey(p => p.ID);
+        });
+
+        modelBuilder.Entity<CommitEntity>(c =>
+        {
+            c.Property(p => p.ID).IsRequired();
+            c.Property(p => p.RID).IsRequired();
+            c.HasKey(p => p.ID);
+            c.HasIndex(p => p.ID).IsUnique();
+            c.HasOne<RepositoryEntity>().WithMany().HasForeignKey(b => b.RID);
+
+        });
     }
     
 
