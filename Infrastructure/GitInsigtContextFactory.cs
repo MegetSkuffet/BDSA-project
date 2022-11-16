c9e7c686103e44bfe7d5b1e1a27dfcfcd91679f1
Malte Alexander Koefoed Schalck <malsc@itu.dk>
2022-11-16 12:17:03 +0100

    med migrations

--- Infrastructure/GitInsigtContextFactory.cs
+++ Infrastructure/GitInsigtContextFactory.cs
@@ -1,6 +1,15 @@
-﻿namespace Infrastructure;
+﻿using Microsoft.EntityFrameworkCore;
 
-public class GitInsigtContextFactory
+
+namespace Infrastructure;
+using Microsoft.EntityFrameworkCore.Design;
+public class GitInsigtContextFactory : IDesignTimeDbContextFactory<GitInsightContext>
 {
-    
+    public GitInsightContext CreateDbContext(string[] args)
+    {
+        var optionBuilder = new DbContextOptionsBuilder<GitInsightContext>();
+        optionBuilder.UseSqlite("Data source=GitInsight.db");
+
+        return new GitInsightContext(optionBuilder.Options);
+    }
 }
