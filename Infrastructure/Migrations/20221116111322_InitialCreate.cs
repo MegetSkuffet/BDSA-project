d1a73a842cbf22f1d26fa6e98bba163060801020
Malte Alexander Koefoed Schalck <malsc@itu.dk>
2022-11-16 12:19:28 +0100

    migrations igen igen

---
+++ Infrastructure/Migrations/20221116111322_InitialCreate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Commits",
+                columns: table => new
+                {
+                    RID = table.Column<string>(type: "TEXT", nullable: false),
+                    CID = table.Column<string>(type: "TEXT", nullable: false),
+                    Author = table.Column<string>(type: "TEXT", nullable: false),
+                    date = table.Column<DateTimeOffset>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commits", x => new { x.RID, x.CID });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Repositories",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "TEXT", nullable: false),
+                    LastCommitSha = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Repositories", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Commits");
+
+            migrationBuilder.DropTable(
+                name: "Repositories");
+        }
+    }
+}
