0313745e88fdc693452ccd1ef3f9d5578297a8db
katrinesando <kmsa@itu.dk>
2022-11-17 13:12:38 +0100

    tried to get forks from repo

--- GitInsight/Controllers/RepositoryController.cs
+++ GitInsight/Controllers/RepositoryController.cs
@@ -1,5 +1,7 @@
 ﻿using GitInsight.Core.Services;
 using Microsoft.AspNetCore.Mvc;
+using Octokit;
+using Repository = LibGit2Sharp.Repository;
 
 
 namespace GitInsight.Controllers;
@@ -31,13 +33,35 @@ public class RepositoryController: Controller
             repoPath = await _cloneService.CloneRepositoryFromWebAsync(user, repository);
         }
 
-        var repo = new Repository(repoPath);
-        //_database.AddRepository(repo);
         
-       // var db = _database.getCommitsPrDay(repo);
+        var repo = new Repository(repoPath);
+        // _database.AddRepository(repo);
+        // var db = _database.getCommitsPrDay(repo);
+        
+        //forks test
+        // var client = new GitHubClient(new ProductHeaderValue(repository));
+        // var userFork = await client.User.Get(user);
+        // Console.WriteLine("------------  {0} has {1} public repositories - go check out their profile at ??-------------",
+        //     userFork.Name,
+        //     userFork.PublicRepos);
+        // var request = new SearchRepositoriesRequest(repository) { Fork = ForkQualifier.IncludeForks };
+        // var result = await client.Search.SearchRepo(request);
+        
         
         //put result of analysis into json instead of current placeholders
         return Json(new{user,repository});
-    }
+        }
+    
+    // public async Task<IActionResult> GetForks(string user, string repository)
+    // {
+    //     var client = new GitHubClient(new ProductHeaderValue(repository));
+    //     var userFork = await client.User.Get(user);
+    //     Console.WriteLine("{0} has {1} public repositories - go check out their profile at ??",
+    //         userFork.Name,
+    //         userFork.PublicRepos);
+    //     var request = new SearchRepositoriesRequest(repository) { Fork = ForkQualifier.IncludeForks };
+    //     var result = await client.Search.SearchRepo(request);
+    //     return Json(new { result });
+    // }
     
 }
