92807065b810a62b92d4df7851aa0b96fa56d401
katrinesando <kmsa@itu.dk>
2022-11-18 13:55:04 +0100

    added 2 different ways to get forks out - with IREADONLY and another JSON

--- GitInsight/Controllers/RepositoryController.cs
+++ GitInsight/Controllers/RepositoryController.cs
@@ -1,4 +1,5 @@
-﻿using GitInsight.Core.Services;
+﻿using System.Runtime.CompilerServices;
+using GitInsight.Core.Services;
 using Microsoft.AspNetCore.Mvc;
 using Octokit;
 using Credentials = Octokit.Credentials;
@@ -13,7 +14,7 @@ public class RepositoryController: Controller
 {
     
     private readonly ICloneService _cloneService;
-    private string? connectionString;
+    private readonly string? _connectionString;
     //private readonly IDatabase _database;
 
     public RepositoryController(ICloneService cloneService)
@@ -23,64 +24,66 @@ public class RepositoryController: Controller
        var configuration = new ConfigurationBuilder()
            .AddUserSecrets<RepositoryController>()
            .Build();
-       connectionString = configuration.GetConnectionString("ConnectionString");
+       _connectionString = configuration.GetConnectionString("ConnectionString");
     }
 
 
     [HttpGet]
     [Route("/{user}/{repository}")]
-    
-    
     public async Task<IActionResult> CloneOrFind(string user, string repository)
     {
-        //safe secret 
-       
-        
         if (!_cloneService.FindRepositoryOnMachine(repository, out var repoPath))
         {
             repoPath = await _cloneService.CloneRepositoryFromWebAsync(user, repository);
         }
 
         var repo = new Repository(repoPath);
+        var forks = await GetForks(user, repository);
+        var forksTest = await GetForksTest(user, repository);
+        var info = forksTest.Select(c =>new{user=c.Owner.Login,repo=c.Name,url=c.GitUrl});
         //_database.AddRepository(repo);
-        
-       // var db = _database.getCommitsPrDay(repo);
-       //forks test
-       int count = 1000;
-       var client = new GitHubClient(new ProductHeaderValue(repository));
-       var tokenAuth = new Credentials(connectionString); // NOTE: not real token
-       client.Credentials = tokenAuth;
-       
-       var request = new SearchRepositoriesRequest(repository) { Fork = ForkQualifier.IncludeForks,User = user};
-       var result = await client.Search.SearchRepo(request);
-       foreach (var item in result.Items)
-       {
-           
-           count = item.ForksCount;
-       }
-       Console.WriteLine("{0} Forks were created --------",count);
-       // Console.WriteLine("https://api.github.com/repos/{0}/{1}/forks",user,repository);
-       // Console.WriteLine(result.Items);
-       
+        // var db = _database.getCommitsPrDay(repo);
+      
         //put result of analysis into json instead of current placeholders
-        return Json(new{count,user,repository,result});
+        return Json(new{user,repository,forks,info});
+    }
+    
+    [Route("/{user}/{repository}")]
+    public async Task<IActionResult> GetForks(string user, string repository)
+    {
+        //forks test
+        var client = new GitHubClient(new ProductHeaderValue(repository));
+        var tokenAuth = new Credentials(_connectionString); // Token created with user-secret
+        client.Credentials = tokenAuth;
+        var forks = await client.Repository.Forks.GetAll(user, repository);
+        var info = forks.Select(c =>new{user=c.Owner.Login,repo=c.Name,url=c.GitUrl});
+        foreach (var item in info)
+        {
+            Console.WriteLine(item);
+        }
+        
+        //put result of analysis into json instead of current placeholders
+        return Json(info);
+    }
+    
+    //Testing purposes
+    [Route("/{user}/{repository}")]
+    public async Task<IReadOnlyList<Octokit.Repository>> GetForksTest(string user, string repository)
+    {
+        //forks test
+        var client = new GitHubClient(new ProductHeaderValue(repository));
+        var tokenAuth = new Credentials(_connectionString); // Token created with user-secret
+        client.Credentials = tokenAuth;
+        var forks = await client.Repository.Forks.GetAll(user, repository);
+        var info = forks.Select(c =>new{user=c.Owner.Login,repo=c.Name,url=c.GitUrl});
+        foreach (var item in info)
+        {
+            Console.WriteLine(item);
+        }
+        
+        //put result of analysis into json instead of current placeholders
+        return forks;
     }
 
-    // public async Task<IActionResult> GetForks(string user, string repository)
-    // {
-    //     var client = new GitHubClient(new ProductHeaderValue(repository));
-    //     var userFork = await client.User.Get(user);
-    //     Console.WriteLine("------------  {0} has {1} public repositories - go check out their profile at ??-------------",
-    //         userFork.Name,
-    //         userFork.PublicRepos
-    //     );
-    //     var request = new SearchRepositoriesRequest(repository) { Fork = ForkQualifier.IncludeForks,User = user};
-    //     var result = await client.Search.SearchRepo(request);
-    //     Console.WriteLine("https://api.github.com/repos/{0}/{1}/forks",user,repository);
-    //     Console.WriteLine(result.Items);
-    //     return Json(new { result });
-    // }
 
-    
-    
 }
