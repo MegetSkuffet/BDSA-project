9a89b24ea8c4948b09fd0b776d7e5d48f2c7d0c2
Philip Mørch <mrphil2105@gmail.com>
2022-12-02 15:08:29 +0100

    Add authentication to services

--- blazor/Program.cs
+++ blazor/Program.cs
@@ -9,4 +9,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001") });
 
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+});
+
 await builder.Build().RunAsync();
